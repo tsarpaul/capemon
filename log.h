ba3c555a53fd838954ad4ecccf87212e2d5c03d7
tsarpaul <21146452+tsarpaul@users.noreply.github.com>
2019-12-05 11:01:45 +0200

    Removed noisy behavior log

--- log.h
+++ log.h
@@ -110,18 +110,18 @@ do { \
 	loq(_index, cat, &__FUNCTION__[4], eval, (ULONG_PTR)ret, fmt, ##__VA_ARGS__); \
 } while (0)
 
-#define LOQ_ntstatus(cat, fmt, ...) _LOQ(NT_SUCCESS(ret), cat, fmt, ##__VA_ARGS__)
-#define LOQ_nonnull(cat, fmt, ...) _LOQ(ret != NULL, cat, fmt, ##__VA_ARGS__)
-#define LOQ_handle(cat, fmt, ...) _LOQ(ret != NULL && ret != INVALID_HANDLE_VALUE, cat, fmt, ##__VA_ARGS__)
-#define LOQ_void(cat, fmt, ...) _LOQ(TRUE, cat, fmt, ##__VA_ARGS__)
-#define LOQ_bool(cat, fmt, ...) _LOQ(ret != FALSE, cat, fmt, ##__VA_ARGS__)
-#define LOQ_hresult(cat, fmt, ...) _LOQ(ret == S_OK, cat, fmt, ##__VA_ARGS__)
-#define LOQ_zero(cat, fmt, ...) _LOQ(ret == 0, cat, fmt, ##__VA_ARGS__)
-#define LOQ_nonzero(cat, fmt, ...) _LOQ(ret != 0, cat, fmt, ##__VA_ARGS__)
-#define LOQ_nonnegone(cat, fmt, ...) _LOQ(ret != -1, cat, fmt, ##__VA_ARGS__)
-#define LOQ_sockerr(cat, fmt, ...) _LOQ(ret != SOCKET_ERROR, cat, fmt, ##__VA_ARGS__)
-#define LOQ_sock(cat, fmt, ...) _LOQ(ret != INVALID_SOCKET, cat, fmt, ##__VA_ARGS__)
-#define LOQ_msgwait(cat, fmt, ...) _LOQ(ret != WAIT_FAILED, cat, fmt, ##__VA_ARGS__)
+#define LOQ_ntstatus(cat, fmt, ...) do { } while(0)
+#define LOQ_nonnull(cat, fmt, ...) do { } while(0)
+#define LOQ_handle(cat, fmt, ...) do { } while(0)
+#define LOQ_void(cat, fmt, ...) do { } while(0)
+#define LOQ_bool(cat, fmt, ...) do { } while(0)
+#define LOQ_hresult(cat, fmt, ...) do { } while(0)
+#define LOQ_zero(cat, fmt, ...) do { } while(0)
+#define LOQ_nonzero(cat, fmt, ...) do { } while(0)
+#define LOQ_nonnegone(cat, fmt, ...) do { } while(0)
+#define LOQ_sockerr(cat, fmt, ...) do { } while(0)
+#define LOQ_sock(cat, fmt, ...) do { } while(0)
+#define LOQ_msgwait(cat, fmt, ...) do { } while(0)
 
 #define ENSURE_LARGE_INTEGER(param) \
     LARGE_INTEGER _##param; _##param.QuadPart = 0; if(param == NULL) param = &_##param
